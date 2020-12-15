8151bc7bcb21aed49d6bc7f6c98e3388ba592436
Piaero <allyft@gmail.com>
2020-12-15 16:40:37 +0100

    Refactored /selections router (beta 2.0)
    - Added merge to recurring champions and merge their score
    
    Minor updates:
    - Update score multipliers
    - Update getting champion counters
    - Added debug console.logs

--- backend/server.js
+++ backend/server.js
@@ -30,20 +30,22 @@ app.get('/champions-list', (req, res) => {
 });
 
 // BUG: After updating state in main section grid, it sends 2 requests to server
+// I am using here 3 kind of objects ontations, 2 types of arrays and one JS Object...
 app.post('/selections', async (req, res) => {
   var myRole = req.body.post.myRole
   championToCounter = req.body.post.enemy[myRole.toLowerCase()];
 
-  let CounterOnLaneMultiplier = 1;
-  let CounterOtherChampionsMultiplier = 0.8;
-  let synergyWithTeammatesMultiplier = 0.45;
+  let counterOnLaneMultiplier = 1;
+  let counterOtherChampionsMultiplier = 0.45;
+  let synergyWithTeammatesMultiplier = 0.20;
 
   // need to decide wheather these should be an object or array
-  let CounterOnLane = [];
-  var CounterOtherChampions = [];
+  let counterOnLane = [];
+  let counterOtherChampions = [];
+  let countersMerged = [];
   let synergyWithTeammates = [];
 
-  let BestCounters = [];
+  let bestCounters = {};
 
   let lanes = ["Top", "Jungle", "Middle", "Bottom", "Support", "Unknown"];
 
@@ -51,40 +53,77 @@ app.post('/selections', async (req, res) => {
   try {
     for (let i = 0; i < lanes.length; i++) {
       let enemyFromLane = req.body.post.enemy[lanes[i].toLowerCase()]
-      console.log(enemyFromLane)
+      console.log(`--- Analising champion and his counters: ${enemyFromLane}`)
 
       if (enemyFromLane !== 'undefined' && enemyFromLane !== "none") {
         await client.db("what2pick").collection('champions').find({ name: enemyFromLane }).toArray()
           .then(results => {
 
-            // Inputting counters from champions from all lanes other than myRole (because there is different score multiplier)
+            // Counters to Other Champions: Inputting counters from champions from all lanes other than myRole (because there is different score multiplier)
             if (lanes[i] !== myRole && results[0].counters[myRole] !== undefined) {
-
               // Iterating trough all champions
               for (let j = 0; j < results[0].counters[myRole].length; j++) {
-                console.log(`Added ${results[0].counters[myRole][j].counter} to CounterOtherChampions Array (from other lanes)`)
-                // console.log(results[0].counters[myRole][j])
-                CounterOtherChampions.push(results[0].counters[myRole][j].counter + " from " + results[0].name)
+                console.log(`Added ${results[0].counters[myRole][j].counter} to counterOtherChampions Array (from other lanes)`)
+                console.log(results[0].counters[myRole][j])
+                results[0].counters[myRole][j].score = results[0].counters[myRole][j].counterRate * counterOtherChampionsMultiplier;
+                results[0].counters[myRole][j].source = "COC";
+                results[0].counters[myRole][j].counterTo = { [enemyFromLane]: results[0].counters[myRole][j].counterRate };
+                counterOtherChampions.push(results[0].counters[myRole][j]);
               }
-              
-              // Inputting counters from champion from my lane (myRole)
+
+              // Counters On Lane: Inputting counters from champion from my lane (myRole)
             } else if (results[0].counters[myRole] !== undefined) {
               // Iterating trough all champions
               for (let j = 0; j < results[0].counters[myRole].length; j++) {
-                console.log(`Added ${results[0].counters[myRole][j].counter} to CounterOtherChampions Array (from myRole lane)`)
-                // console.log(results[0].counters[myRole][j])
-                CounterOtherChampions.push(results[0].counters[myRole][j].counter + " from " + results[0].name)
+                console.log(`Added ${results[0].counters[myRole][j].counter} to counterOtherChampions Array (from myRole lane)`)
+                results[0].counters[myRole][j].score = results[0].counters[myRole][j].counterRate * counterOnLaneMultiplier;
+                results[0].counters[myRole][j].source = "COL";
+                results[0].counters[myRole][j].counterTo = { [enemyFromLane]: results[0].counters[myRole][j].counterRate };
+                counterOnLane.push(results[0].counters[myRole][j]);
               }
             }
           })
           .catch(error => console.error(error))
       }
-    }
-    console.log(`BELOW CounterOtherChampions`)
-    console.log(CounterOtherChampions)
-    console.log(`ABOVE CounterOtherChampions`)
+    } // end of populating counterOnLane and counterOtherChampions arrays
 
-    res.json(`${CounterOtherChampions}`)
+    countersMerged = counterOnLane.concat(counterOtherChampions)
+
+    for (let i = 0; i < countersMerged.length; i++) {
+      if (bestCounters.hasOwnProperty(countersMerged[i].counter)) {
+        console.log(countersMerged[i].counter)
+        bestCounters[countersMerged[i].counter].score += countersMerged[i].score
+        bestCounters[countersMerged[i].counter].counterTo[Object.keys(countersMerged[i].counterTo)[0]] = {
+          counterRate: countersMerged[i].counterRate,
+          source: countersMerged[i].source
+        }
+
+      } else {
+        bestCounters[countersMerged[i].counter] = {
+          score: countersMerged[i].score,
+          counterTo: {
+            [Object.keys(countersMerged[i].counterTo)[0]]: {
+              counterRate: countersMerged[i].counterRate,
+              source: countersMerged[i].source
+            }
+          }
+        }
+      }
+    }
+
+    let bestCountersorted = Object.entries(bestCounters).sort((a, b) => (a[1].score < b[1].score) ? 1 : -1);
+
+    console.log(`------------ BELOW countersMerged myRole is: ${myRole}`)
+    console.log(countersMerged)
+    console.log(`------------ ABOVE countersMerged`)
+
+
+    console.log(`------------ BELOW bestCounters myRole is: ${myRole}`)
+    console.log(bestCountersorted)
+    console.log(`------------ ABOVE bestCounters`)
+
+    res.json(`${JSON.stringify(bestCounters)}`)
+    
   } catch (error) {
     console.log(error);
   }
