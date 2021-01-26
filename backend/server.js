c55dd4976854d6fb70dd189fefa7002b0e995d36
Piaero <allyft@gmail.com>
2021-01-26 23:11:25 +0100

    Refactor functions that removed champions unplayade on my selected role.

--- backend/server.js
+++ backend/server.js
@@ -40,13 +40,14 @@ app.post('/selections', async (req, res) => {
 
   let lanes = ["Top", "Jungle", "Middle", "Bottom", "Support", "Unknown"];
 
+  // maybe needs to move inside function
   var queryForAvoidToCounter = `counters.${myRole}.counter`
   var projectionForAvoidToCounter = `counters.${myRole}.counter.$`
 
   var picksProposition = {}
   var avoidToPickProposition = {}
 
-  function pushPicksIntoPicksProposition(championName, results, isMyRole, isCounterOrSynergy) {
+  function pushPicksIntoPicksProposition(championName, results, isMyRole, lane, isCounterOrSynergy) {
 
     let counterSynergyMultiplier = isCounterOrSynergy === "counters" ? isMyRole ? counterOnMyLaneMultiplier : counterOtherLaneMultiplier : synergyWithTeammatesMultiplier
     let counterOrSynergy = isCounterOrSynergy === "counters" ? "counter" : "synergy"
@@ -59,7 +60,7 @@ app.post('/selections', async (req, res) => {
           picksProposition[results[j][counterOrSynergy]].score += results[j][counterRateOrSynergyRate] * counterSynergyMultiplier
           picksProposition[results[j][counterOrSynergy]][counterToOrSynergyTo][championName] = {
             [counterRateOrSynergyRate]: results[j][counterRateOrSynergyRate],
-            source: lanes[i],
+            source: lane,
           }
         } else {
           picksProposition[results[j][counterOrSynergy]] = {
@@ -70,7 +71,7 @@ app.post('/selections', async (req, res) => {
           picksProposition[results[j][counterOrSynergy]][counterToOrSynergyTo] = {
             [championName]: {
               [counterRateOrSynergyRate]: results[j][counterRateOrSynergyRate],
-              source: lanes[i],
+              source: lane,
             }
           }
         }
@@ -78,7 +79,7 @@ app.post('/selections', async (req, res) => {
     }
   }
 
-  function pushAvoidToPickIntoAvoidsProposition(championName, results, isMyRole) {
+  function pushAvoidToPickIntoAvoidsProposition(championName, results, isMyRole, lane) {
 
     let counterSynergyMultiplier = isMyRole ? counterOnMyLaneMultiplier : counterOtherLaneMultiplier
 
@@ -88,7 +89,7 @@ app.post('/selections', async (req, res) => {
           avoidToPickProposition[results[j].name].score += results[j].counters[myRole][0].counterRate * counterSynergyMultiplier
           avoidToPickProposition[results[j].name].counterTo[championName] = {
             counterRate: results[j].counters[myRole][0].counterRate,
-            source: lanes[i]
+            source: lane,
           }
         } else {
           avoidToPickProposition[results[j].name] = {
@@ -96,7 +97,7 @@ app.post('/selections', async (req, res) => {
             counterTo: {
               [championName]: {
                 counterRate: results[j].counters[myRole][0].counterRate,
-                source: lanes[i]
+                source: lane,
               }
             }
           }
@@ -104,6 +105,30 @@ app.post('/selections', async (req, res) => {
       }
     }
   }
+
+  async function removeChampionsUnplayableOnMyLane(proposition) {
+    let queryForChampionsToCheck = []
+
+    for (let i = 0; i < Object.keys(proposition).length; i++) {
+      let tempObject = { name: Object.keys(proposition)[i] }
+      queryForChampionsToCheck.push(tempObject)
+    }
+
+    if (queryForChampionsToCheck.length) {
+      await client.db("what2pick").collection('champions').find({ $or: queryForChampionsToCheck }).project({ name: 1, laneType: 1 }).toArray()
+        .then(results => {
+          for (let i = 0; i < results.length; i++) {
+            if (!results[i].laneType.includes(myRole)) {
+              delete proposition[results[i].name]
+            }
+          }
+        })
+        .catch(error => console.error(error))
+    }
+    return proposition
+  }
+
+  new Promise(async function (resolve, reject) {
 
     for (var i = 0; i < lanes.length; i++) {
       let enemyFromLane = req.body.post.enemy[lanes[i].toLowerCase()]
@@ -116,19 +141,19 @@ app.post('/selections', async (req, res) => {
 
           await client.db("what2pick").collection('champions').find({ name: enemyFromLane }).project({ name: 1, counters: 1 }).toArray()
             .then(results => {
-              pushPicksIntoPicksProposition(enemyFromLane, results[0] && results[0].counters && results[0].counters[myRole], false, "counters")
+              pushPicksIntoPicksProposition(enemyFromLane, results[0] && results[0].counters && results[0].counters[myRole], false, lanes[i], "counters")
             })
             .catch(error => console.error(error))
 
           await client.db("what2pick").collection('champions').find({ [queryForAvoidToCounter]: enemyFromLane }).project({ [projectionForAvoidToCounter]: 1, name: 1 }).toArray()
             .then(results => {
-              pushAvoidToPickIntoAvoidsProposition(enemyFromLane, results, false)
+              pushAvoidToPickIntoAvoidsProposition(enemyFromLane, results, false, lanes[i])
             })
             .catch(error => console.error(error))
 
           await client.db("what2pick").collection('champions').find({ name: teammateFromLane }).project({ name: 1, synergies: 1 }).toArray()
             .then(results => {
-              pushPicksIntoPicksProposition(teammateFromLane, results[0] && results[0].synergies, null, "synergies")
+              pushPicksIntoPicksProposition(teammateFromLane, results[0] && results[0].synergies, null, lanes[i], "synergies")
             })
             .catch(error => console.error(error))
 
@@ -136,69 +161,35 @@ app.post('/selections', async (req, res) => {
         } else if (myRole !== "none" && myRole !== undefined) {
           await client.db("what2pick").collection('champions').find({ name: enemyFromLane }).project({ name: 1, counters: 1 }).toArray()
             .then(results => {
-              pushPicksIntoPicksProposition(enemyFromLane, results[0] && results[0].counters && results[0].counters[myRole], true, "counters")
+              pushPicksIntoPicksProposition(enemyFromLane, results[0] && results[0].counters && results[0].counters[myRole], true, lanes[i], "counters")
             })
             .catch(error => console.error(error))
 
           await client.db("what2pick").collection('champions').find({ [queryForAvoidToCounter]: enemyFromLane }).project({ [projectionForAvoidToCounter]: 1, name: 1 }).toArray()
             .then(results => {
-              pushAvoidToPickIntoAvoidsProposition(enemyFromLane, results, true)
+              pushAvoidToPickIntoAvoidsProposition(enemyFromLane, results, true, lanes[i])
             })
             .catch(error => console.error(error))
         }
       }
     } // End of iterating trough all lanes
-
-    // Remove from Counter propositions champions that aren't playable (laneType) on myRole
-    let queryForRemovalCountersFromOtherLanes = []
-
-    for (let i = 0; i < Object.keys(picksProposition).length; i++) {
-      let tempObject = { name: Object.keys(picksProposition)[i] }
-      queryForRemovalCountersFromOtherLanes.push(tempObject)
+    resolve("resolved")
+  }).then(async function () {
+    let picksAndAvoidsProposition = {
+      bestPicks: await removeChampionsUnplayableOnMyLane(picksProposition),
+      bestAvoids: await removeChampionsUnplayableOnMyLane(avoidToPickProposition)
     }
 
-    if (queryForRemovalCountersFromOtherLanes.length) {
-      await client.db("what2pick").collection('champions').find({ $or: queryForRemovalCountersFromOtherLanes }).project({ name: 1, laneType: 1 }).toArray()
-        .then(results => {
-          for (let i = 0; i < results.length; i++) {
-            if (!results[i].laneType.includes(myRole)) {
-              delete picksProposition[results[i].name]
-            }
-          }
-        })
-        .catch(error => console.error(error))
-    }
-
-    // Remove from Avoid to Pick propositions champions that aren't playable (laneType) on myRole
-    let queryForRemovalAvoidsFromOtherLanes = []
-
-    for (let i = 0; i < Object.keys(avoidToPickProposition).length; i++) {
-      let tempObject = { name: Object.keys(avoidToPickProposition)[i] }
-      queryForRemovalAvoidsFromOtherLanes.push(tempObject)
-    }
-
-    if (queryForRemovalAvoidsFromOtherLanes.length) {
-      await client.db("what2pick").collection('champions').find({ $or: queryForRemovalAvoidsFromOtherLanes }).project({ name: 1, laneType: 1 }).toArray()
-        .then(results => {
-          for (let i = 0; i < results.length; i++) {
-            if (!results[i].laneType.includes(myRole)) {
-              delete avoidToPickProposition[results[i].name]
-            }
-          }
-        })
-        .catch(error => console.error(error))
-    }
-
-    // Adjust score of Champions that are both in "avoidToPickFromAllLanes" and "CountersProposition"
-    for (let i = 0; i < Object.entries(avoidToPickProposition).length; i++) {
-      if (picksProposition.hasOwnProperty(Object.entries(avoidToPickProposition)[i][0])) {
-        picksProposition[Object.entries(avoidToPickProposition)[i][0]].score -= avoidToPickProposition[Object.entries(avoidToPickProposition)[i][0]].score
+    // Adjust score of Champions that are both in "bestPicks" and "bestAvoids"
+    for (let i = 0; i < Object.entries(picksAndAvoidsProposition.bestAvoids).length; i++) {
+      if (picksAndAvoidsProposition.bestPicks.hasOwnProperty(Object.entries(picksAndAvoidsProposition.bestAvoids)[i][0])) {
+        picksAndAvoidsProposition.bestPicks[Object.entries(picksAndAvoidsProposition.bestAvoids)[i][0]].score -= picksAndAvoidsProposition.bestAvoids[Object.entries(picksAndAvoidsProposition.bestAvoids)[i][0]].score
       }
     }
 
     let response = {
-      bestCountersSorted: Object.entries(picksProposition).sort((a, b) => (a[1].score < b[1].score) ? 1 : -1),
-      bestAvoidSorted: Object.entries(avoidToPickProposition).sort((a, b) => (a[1].score < b[1].score) ? 1 : -1)
+      bestCountersSorted: Object.entries(picksAndAvoidsProposition.bestPicks).sort((a, b) => (a[1].score < b[1].score) ? 1 : -1),
+      bestAvoidSorted: Object.entries(picksAndAvoidsProposition.bestAvoids).sort((a, b) => (a[1].score < b[1].score) ? 1 : -1)
     }
 
     console.log(`------------------------req.body.post-------------------------------------`)
@@ -210,18 +201,17 @@ app.post('/selections', async (req, res) => {
     console.log(`------------------------RESPONSE-------------------------------------`)
 
     res.json(response)
-
-
+  })
 })
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, '..', 'frontend/build')));
+  if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '..', 'frontend/build')));
 
-  // Handle React routing, return all requests to React app
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '..', 'frontend/build', 'index.html'));
-  });
-}
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+      res.sendFile(path.join(__dirname, '..', 'frontend/build', 'index.html'));
+    });
+  }
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+  app.listen(port, () => console.log(`Listening on port ${port}`));
